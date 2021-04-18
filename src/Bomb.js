d6581203859b87be0c212deaea954650dc2c03a7
Jonah <benjaminjmallard@gmail.com>
2021-04-18 16:16:44 -0500

    commit tree 7d1f5948952bd3765ec6f2921634c2f114f07c46

--- src/Bomb.js
+++ src/Bomb.js
@@ -1,1 +1,10 @@
 // your Bomb code here!
+import React from 'react'
+
+export default function Bomb() {
+  return (
+    <div>
+      <h1>Bomb</h1>
+    </div>
+  )
+}
--- src/ImageSlider.js
+++ src/ImageSlider.js
@@ -1,1 +1,10 @@
 // your ImageSlider code here!
+import React from 'react'
+
+export default function ImageSlider() {
+    return (
+        <div>
+            <h1>ImageSlider</h1>
+        </div>
+    )
+}
